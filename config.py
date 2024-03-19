f49eee3459937504ee310f3a6f9cea2970b66b4e
Seeutech <118717438+Seeutech@users.noreply.github.com>
2024-03-19 08:02:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,40 @@
-api_id =  #my.telegram.org
-api_hash = "" #my.telegram.org
+api_id = 23054736 #my.telegram.org
+api_hash = "d538c2e1a687d414f5c3dce7bf4a743c" #my.telegram.org
 
-bot_token = ""
+bot_token = "6758593032:AAFm0eyeXd0jNUdwZd11I8oEoPASAEgdIJU"
 
-CHAT_ID = -100 #chatid and make sure add bot on this chat and promote as admin 
+CHAT_ID = -1002023393491 #chatid and make sure add bot on this chat and promote as admin 
 
 
 cookies = {
+    'csrfToken': 'Tj8SwJNs6rcbNxSeu0iJXcuw',
+    'browserid': 'PutoLSrikIu91W1REHoJLEEhJsjNp2HQlBpl2bvbmjTSI-kccWC7TvuxJ88=',
+    'lang': 'en',
+    '__bid_n': '18ce2a5d2f4cd311ec4207',
+    '_ga': 'GA1.1.706358978.1704609570',
+    '__stripe_mid': '1231e2b1-ff34-44e0-a973-5801dd84a3a2662c38',
+    '__stripe_sid': 'c874932f-5f69-4946-9bab-202e047e12bf46fadf',
+    'g_state': '{"i_l":0}',
+    'ndus': 'YVTP1CCteHui93gSFn3vtXB7Wd336q-99yFauQI2',
+    'ndut_fmt': '67727484E1F6CFCB7B433E53007BBF2FE4F5C09F1F1819D0344EDF4251F5ADCE',
+    '_ga_06ZNKL8C2E': 'GS1.1.1704609570.1.1.1704609953.60.0.0',
 }
 
-headers = {}
+headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    # 'Cookie': 'csrfToken=Tj8SwJNs6rcbNxSeu0iJXcuw; browserid=PutoLSrikIu91W1REHoJLEEhJsjNp2HQlBpl2bvbmjTSI-kccWC7TvuxJ88=; lang=en; __bid_n=18ce2a5d2f4cd311ec4207; _ga=GA1.1.706358978.1704609570; __stripe_mid=1231e2b1-ff34-44e0-a973-5801dd84a3a2662c38; __stripe_sid=c874932f-5f69-4946-9bab-202e047e12bf46fadf; g_state={"i_l":0}; ndus=YVTP1CCteHui93gSFn3vtXB7Wd336q-99yFauQI2; ndut_fmt=67727484E1F6CFCB7B433E53007BBF2FE4F5C09F1F1819D0344EDF4251F5ADCE; _ga_06ZNKL8C2E=GS1.1.1704609570.1.1.1704609953.60.0.0',
+    'Referer': 'https://www.terabox1024.com/recyclebin',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-User': '?1',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+}
+
